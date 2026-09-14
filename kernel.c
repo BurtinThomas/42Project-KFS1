e713e16743f3e781dbada8010ab46d1412f0056a
tburtin <tburtin@f6r12s7.paris.42.school>
2026-09-14 15:19:18 +0200

    vgerger

--- kernel.c
+++ kernel.c
@@ -5,13 +5,8 @@
 #error "This needs to be compiled with an ix86-elf compiler"
 #endif
 
-enum vga_color {
-    VGA_COLOR_BLACK = 0,
-    VGA_COLOR_LIGHT_GREY = 7
-};
-
-static inline uint8_t compose_vga_color(enum vga_color foreground,
-                                        enum vga_color background)
+static inline uint8_t compose_vga_color(uint8_t foreground,
+                                        uint8_t background)
 {
     return foreground | (background << 4);
 }
@@ -21,22 +16,19 @@ static inline uint16_t compose_vga_entry(unsigned char character, uint8_t color)
     return (uint16_t)character | ((uint16_t)color << 8);
 }
 
-#define VGA_WIDTH  80
-#define VGA_HEIGHT 25
-#define VGA_MEMORY 0xB8000
 
 size_t terminal_row = 0;
 size_t terminal_column = 0;
 uint8_t terminal_color;
-uint16_t* terminal_buffer = (uint16_t*)VGA_MEMORY;
+uint16_t* terminal_buffer = (uint16_t*)0xB8000;
 
 void initialize_terminal(void)
 {
-    terminal_color = compose_vga_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+    terminal_color = compose_vga_color(7, 0);
 
-    for (size_t row = 0; row < VGA_HEIGHT; row++) {
-        for (size_t column = 0; column < VGA_WIDTH; column++) {
-            terminal_buffer[row * VGA_WIDTH + column] =
+    for (size_t row = 0; row < 25; row++) {
+        for (size_t column = 0; column < 80; column++) {
+            terminal_buffer[row * 80 + column] =
                 compose_vga_entry(' ', terminal_color);
         }
     }
@@ -46,7 +38,7 @@ void write_terminal_character(char c)
     if (c == '\n') {
         terminal_column = 0;
 
-        if (terminal_row + 1 == VGA_HEIGHT)
+        if (terminal_row + 1 == 25)
             terminal_row = 0;
         else
             terminal_row++;
@@ -54,15 +46,15 @@ void write_terminal_character(char c)
         return;
     }
 
-    terminal_buffer[terminal_row * VGA_WIDTH + terminal_column] =
+    terminal_buffer[terminal_row * 80 + terminal_column] =
         compose_vga_entry(c, terminal_color);
 
     terminal_column++;
 
-    if (terminal_column == VGA_WIDTH) {
+    if (terminal_column == 80) {
         terminal_column = 0;
 
-        if (terminal_row + 1 == VGA_HEIGHT)
+        if (terminal_row + 1 == 25)
             terminal_row = 0;
         else
             terminal_row++;
